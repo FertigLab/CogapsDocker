eadc8d6725f1e57259763f690184bb1b693111b1
Tom Sherman <tomsherman159@gmail.com>
2019-04-22 21:55:39 -0400

    missing line separator

--- src/aws_cogaps.sh
+++ src/aws_cogaps.sh
@@ -111,9 +111,9 @@ Rscript -e "\
     params <- setParam(params, \"nPatterns\", ${GAPS_N_PATTERNS}); \
     params <- setDistributedParams(params, nSets=${GAPS_N_SETS}); \
     params <- setParam(params, \"distributed\", \"${GAPS_DISTRIBUTED_METHOD}\"); \
-    nThreads <- ${GAPS_N_THREADS} \
-    if (is.null(params@distributed) && nThreads == 1) ; \
-        nThreads <- parallel::detectCores() ; \
+    nThreads <- ${GAPS_N_THREADS}; \
+    if (is.null(params@distributed) && nThreads == 1); \
+        nThreads <- parallel::detectCores(); \
     gapsResult <- CoGAPS::CoGAPS(data=\"${LOCAL_DATA_FILE}\", \
         params=params, nIterations=${GAPS_N_ITERATIONS}, seed=${GAPS_SEED}, \
         nThreads=nThreads, outputFrequency=${GAPS_OUTPUT_FREQUENCY}, \
